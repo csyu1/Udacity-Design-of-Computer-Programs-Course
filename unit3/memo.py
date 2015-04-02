d909b7cf2d614be1351f1de59e1d3f5386680afb
csyu1 <carl.earvin.yu@gmail.com>
2015-04-03 07:43:26 +0800

    caching results decorator

---
+++ unit3/memo.py
@@ -0,0 +1,11 @@
+def memo(f):
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args)
+            return result
+        except TypeError:
+            return f(args)
+    return _f
